00d23c8166860a48af45477074b7c1fb53ccc007
AlexanderKovalenko <alexander.kovalenko@devexpress.com>
2021-02-16 14:08:05 +0300

    Update RouteConfig.vb

--- VB/MvcThrowCustomExceptionDashboardErrorToast/App_Start/RouteConfig.vb
+++ VB/MvcThrowCustomExceptionDashboardErrorToast/App_Start/RouteConfig.vb
@@ -11,7 +11,7 @@ Namespace MvcThrowCustomExceptionDashboardErrorToast
 		Public Shared Sub RegisterRoutes(ByVal routes As RouteCollection)
 			routes.IgnoreRoute("{resource}.axd/{*pathInfo}")
 
-			routes.MapRoute(name:= "Default", url:= "{controller}/{action}/{id}", defaults:= New With {Key .controller = "Home", Key .action = "Index", Key .id = UrlParameter.Optional})
+			routes.MapRoute(name:= "Default", url:= "{controller}/{action}/{id}", defaults:= New With {Key .controller = "Default", Key .action = "Index", Key .id = UrlParameter.Optional})
 		End Sub
 	End Class
 End Namespace
